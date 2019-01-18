863e1eb22ea66aa06dffc53a0c5e7f3565a2df53
Ariel Hicks <hicks.ariel@gmail.com>
2019-01-18 18:42:09 +0000

    Automatically backed up by Learn

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@ Student.create(name: "Luke", grade: "9th")
 Student.create(name: "Devon", grade: "11th")
 Student.create(name: "Sarah", grade: "10th")
 
+
 namespace :db do
   ...
   desc 'seed the database with some dummy data'
